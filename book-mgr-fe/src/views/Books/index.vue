a305d490fb18f716ea2cf33f850faa31ff3f29bd
coderWuqb <729522422@qq.com>
2021-01-27 15:26:01 +0800

    前端图书管理基础界面

--- book-mgr-fe/src/views/Books/index.vue
+++ book-mgr-fe/src/views/Books/index.vue
@@ -1,15 +1,70 @@
 <template>
     <div>
-        book
+        <!-- Ant Design 卡片 -->
+        <a-card>
+            <h2>图书列表</h2>
+
+            <!-- Ant Design 下划线 -->
+            <a-divider/>
+
+            <div class="app-search">
+                <div class="search">
+                    <!-- Ant Design 搜索框 -->
+                    <a-input-search placeholder="根据书名搜索" enter-button/>
+                </div>
+                <a-button>添加一条</a-button>
+            </div>
+            
+            <a-divider/>
+            
+            <!-- Ant Design 表格 -->
+            <a-table :columns="columns" :data-source="dataSource">
+
+            </a-table>
+
+        </a-card>
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+import { defineComponent } from 'vue';
+export default defineComponent({
+    setup(){
+        // 表格属性名
+        const columns = [
+            {
+                title: '名字',
+                dataIndex: 'name',
+            },
+            {
+                title: '年龄',
+                dataIndex: 'age',
+            },
+        ];
+
+        // 数据源 
+        const dataSource = [
+            {
+                name: '小红',
+                age: '18',
+            },
+        ];
+
+        return{
+            columns,
+            dataSource,
+        }
+    },
+});
 </script>
 
 <style lang='scss' scoped>
+    .app-search{
+        display: flex;
+        justify-content: space-between;
 
+        .search{
+        width:200px;
+        }
+    }
 </style>
